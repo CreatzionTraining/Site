69a72b222d30c7b5cd78fdd23ca17e4e7295ff2b
Sathiya2144 <sathiya8747@gmail.com>
2025-12-04 21:52:53 +0530

    wasted

---
+++ .next/dev/build/chunks/[turbopack-node]_transforms_postcss_ts_f3d06558._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/OneDrive/Desktop/Company_Portfolio/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "chunks/bd12c_df014c2e._.js",
+  "chunks/[root-of-the-server]__31482945._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/OneDrive/Desktop/Company_Portfolio/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
---
+++ .next/dev/static/chunks/OneDrive_Desktop_Company_Portfolio_a0ff3932._.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_7f877b22._.js",
+  "static/chunks/bd12c_next_dist_compiled_react-dom_9b3dfcb2._.js",
+  "static/chunks/bd12c_next_dist_compiled_react-server-dom-turbopack_99155b37._.js",
+  "static/chunks/bd12c_next_dist_compiled_next-devtools_index_7b946d1f.js",
+  "static/chunks/bd12c_next_dist_compiled_430703c2._.js",
+  "static/chunks/bd12c_next_dist_client_7215dc2f._.js",
+  "static/chunks/bd12c_next_dist_e922f8e3._.js",
+  "static/chunks/bd12c_@swc_helpers_cjs_76424855._.js"
+],
+    source: "entry"
+});
