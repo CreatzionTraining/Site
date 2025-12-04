1442be4329424db8bcfdc7fa0a3bfe88529eb86c
bhuvan771 <bhuvankumar13995@gmail.com>
2025-12-04 21:23:54 +0530

    testing

---
+++ .next/dev/build/chunks/[turbopack-node]_transforms_postcss_ts_a86be61c._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Desktop/Site/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "chunks/24f09_bba5cb14._.js",
+  "chunks/[root-of-the-server]__7118c0b3._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Desktop/Site/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
---
+++ .next/dev/static/chunks/Desktop_Site_app_favicon_ico_mjs_f3b8a6e9._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/24f09_next_dist_0eb14aef._.js"
+],
+    source: "dynamic"
+});
