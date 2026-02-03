200ae3c73dd07a5aef9405e2b9b0e04fbf915bed
RUBESHR7 <jarvistechnology1.com>
2026-02-03 22:14:20 +0530

    pull

--- .next/dev/server/next-font-manifest.js
+++ .next/dev/server/next-font-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/Desktop/Site/app/about/page\": [\n      \"static/media/83afe278b6a6bb3c-s.p.3a6ba036.woff2\"\n    ],\n    \"[project]/Desktop/Site/app/page\": [\n      \"static/media/83afe278b6a6bb3c-s.p.3a6ba036.woff2\"\n    ]\n  },\n  \"appUsingSizeAdjust\": true,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
-=======
 self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/Desktop/Site/app/page\": [\n      \"static/media/83afe278b6a6bb3c-s.p.3a6ba036.woff2\"\n    ]\n  },\n  \"appUsingSizeAdjust\": true,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
->>>>>>> 84c51d82a52743542424244e9bfdc8754f60c03d
