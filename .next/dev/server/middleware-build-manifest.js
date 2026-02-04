ccf13a693cdbc2c484a16e54994e5a0aaa530ca2
Sathiya2144 <sathiya8747@gmail.com>
2026-02-04 17:39:07 +0530

    redesign Contact page2

--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -1,31 +1,6 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/_app": [
-      "static/chunks/b53d4_next_dist_compiled_3ab8c4fe._.js",
-      "static/chunks/b53d4_next_dist_shared_lib_cedbe552._.js",
-      "static/chunks/b53d4_next_dist_client_23bb32d2._.js",
-      "static/chunks/b53d4_next_dist_f650a05a._.js",
-      "static/chunks/b53d4_next_app_02fabb6e.js",
-      "static/chunks/[next]_entry_page-loader_ts_af342fd3._.js",
-      "static/chunks/b53d4_react-dom_c4846a18._.js",
-      "static/chunks/b53d4_d82c1244._.js",
-      "static/chunks/[root-of-the-server]__9f60833b._.js",
-      "static/chunks/OneDrive_Desktop_Site_pages__app_2da965e7._.js",
-      "static/chunks/turbopack-OneDrive_Desktop_Site_pages__app_f2e0fcc7._.js"
-    ],
-    "/_error": [
-      "static/chunks/b53d4_next_dist_compiled_3ab8c4fe._.js",
-      "static/chunks/b53d4_next_dist_shared_lib_374be23d._.js",
-      "static/chunks/b53d4_next_dist_client_23bb32d2._.js",
-      "static/chunks/b53d4_next_dist_6204a93f._.js",
-      "static/chunks/b53d4_next_error_5eb656af.js",
-      "static/chunks/[next]_entry_page-loader_ts_7ddec93e._.js",
-      "static/chunks/b53d4_react-dom_c4846a18._.js",
-      "static/chunks/b53d4_d82c1244._.js",
-      "static/chunks/[root-of-the-server]__f3777010._.js",
-      "static/chunks/OneDrive_Desktop_Site_pages__error_2da965e7._.js",
-      "static/chunks/turbopack-OneDrive_Desktop_Site_pages__error_d94ec954._.js"
-    ]
+    "/_app": []
   },
   "devFiles": [],
   "polyfillFiles": [
