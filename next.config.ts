0e8a3e99bc70875a6ce2c6fe9c957306199161a3
Bhuvan Kumar <Bhuvankumar13395@gmail.com>
2026-02-11 20:53:46 +0530

    sd

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['@splinetool/react-spline'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
 };
 
 export default nextConfig;
